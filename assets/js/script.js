b5930deb503014f06266b55e97ac8ab3723a3a08
Alan Manoel <alanmanoel517@gmail.com>
2023-09-11 15:28:07 -0300

    add responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-const btn = document.querySelector('#button');
-const inputName = document.querySelector('#name');
-const inputEmail = document.querySelector('#email');
-const inputsubject = document.querySelector('#subject');
-
-
 const forms = document.querySelector('#form');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuMobile.addEventListener('click', function(){
+    const navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('active');
+});
+
 forms.addEventListener('submit', function (event) {
+  const btn = document.querySelector('#button');
+  const inputName = document.querySelector('#name');
+  const inputEmail = document.querySelector('#email');
+  const inputsubject = document.querySelector('#subject');
+
   event.preventDefault();
   if (inputName.value == "") {
     alert("Campo nome vazio!")
@@ -17,18 +23,19 @@ forms.addEventListener('submit', function (event) {
     alert("Campo assunto vazio!")
   }
   else {
-    btn.value = 'Sending...';
-
+    btn.innerHTML ='Enviando...';
+    
     const serviceID = 'default_service';
     const templateID = 'template_ixohtie';
 
     emailjs.sendForm(serviceID, templateID, this)
       .then(() => {
         btn.value = 'Send Email';
-        alert('Menssagem enviada.');
+        alert('Mensagem enviada.');
         inputName.value = "";
         inputEmail.value = "";
         inputsubject.value = "";
+        btn.innerHTML = "Enviar"
       }, (err) => {
         btn.value = 'Send Email';
         alert(JSON.stringify(err));
